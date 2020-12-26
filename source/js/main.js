3f046f51a035798f46e360bc70efe5c65353aa26
Aleksei Bugaev <103301@mail.ru>
2020-12-26 07:56:35 +0300

    добавляет плавного появления нижних карточек

--- source/js/main.js
+++ source/js/main.js
@@ -9,3 +9,13 @@ new Swiper('.slider__container', {
 		prevEl: '.slider__btn--back',
 	},
 })
+
+const listItem = document.querySelectorAll('.service__list li');
+let delay = 0;
+
+window.addEventListener('load', () => {
+  listItem.forEach(item => {
+    item.style.animationDelay = `${delay += 0.1}s`;
+    item.classList.add('fade-in')
+  })
+});
